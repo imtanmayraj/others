03dd071d10a1db678d2c698472bf059f723ce10d
imtanmayraj <tanmaymth@gmail.com>
2024-10-02 19:52:25 +0530

    exception

--- Demo.java
+++ Demo.java
@@ -1,24 +1,40 @@
-@FunctionalInterface
-interface A
-{
-	int add(int i, int j);
-}
+import java.util.*;
 
 
 public class Demo {
-    public static void main(String[] args) {
-//    	
-//   	A obj=new A()
-//   	{
-//   		public int add(int i, int j)
-//   		{
-//   			return i+j;
-//   		}
-//   	};
+    public static void main(String[] args) throws NumberFormatException {   
     	
-    	A obj=(i,j) -> i+j;
-   	
-   	int result=obj.add(5, 4);
-   	System.out.println(result);
+    	int i=0;
+    	int j=0;
+    	try
+    	{
+    		j=18/i;
+    		
+    	}
+    	catch(Exception e)
+    	{
+    		System.out.println("Someting went wrong.");
+    		// System.out.println("Bye");
+    	}
+    	// finally
+    	// {
+    		System.out.println("Bye");
+    	// }
+    	
+    	
+//     	int num=0;
+//     	//BufferedReader br=null;
+//     	try(BufferedReader br=new BufferedReader(new InputStreamReader(System.in)))
+//     	{
+// //    		InputStreamReader in =new InputStreamReader(System.in);
+// //    		BufferedReader br=new BufferedReader(in);
+//     		num=Integer.parseInt(br.readLine());
+//     		System.out.println(num);
+    	// }
+    	// finally
+    	// {
+    	// 	//br.close();
+    	// }
     }
+    
 }
