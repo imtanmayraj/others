5553ab27858be00de30b0c1f0567c6a1e6352455
imtanmayraj <tanmaymth@gmail.com>
2024-10-02 23:09:20 +0530

    exception throw keyword

--- Demo.java
+++ Demo.java
@@ -2,16 +2,16 @@ public class Demo {
     public static void main(String[] args) {
     	
 //    	int i=2;
-    	int i=0;
+    	int i=20;
     	int j=0;
  
     	
     	try
     	{
     		j=18/i;
-    	// 	if(j==0)
-    	// 		throw new ArithmeticException("I don't want to do print zero");
-    	// 
+    		if(j==0)
+    			throw new ArithmeticException("I don't want to do print zero");
+    	
         }
 
     	catch(ArithmeticException e)
