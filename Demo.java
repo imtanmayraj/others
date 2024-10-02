c226b28d904f704161ef6c5d74a887df27226c5f
imtanmayraj <tanmaymth@gmail.com>
2024-10-02 20:10:56 +0530

    try with multiple catch block

--- Demo.java
+++ Demo.java
@@ -1,19 +1,39 @@
 public class Demo {
     public static void main(String[] args) {
-    
-    int i=0;
-    int j=0;
-   
-    try
-    {
-    	j=18/i;
-    }
-    catch(Exception e)
-    {
-    	System.out.println("Something went wrong");
-    }
-    System.out.println(j);
-    
-    System.out.println("Bye");
+    	
+    	int i=2;
+  //  	int i=0;
+    	int j=0;
+    	
+    	int nums[]=new int[5];
+    	String str=null;
+    	
+    	try
+    	{
+    		j=18/i;
+    		System.out.println(str.length());
+    		System.out.println(nums[1]);
+    		System.out.println(nums[5]);
+    	}
+   	// catch(Exception e)
+   	// {
+ 
+   	// 	System.out.println("can't divide by zero "+e);
+   	// }
+    	catch(ArithmeticException e)
+    	{
+    		System.out.println("Cannot divide by zero");
+    	}
+    	catch(ArrayIndexOutOfBoundsException e)
+    	{
+    		System.out.println("Stay in your limit.");
+    	}
+    	catch(Exception e)
+    	{
+  
+    		System.out.println("Something went wrong."+e);
+    	}
+    	System.out.println(j);
+    	System.out.println("Bye");
     }
 }
