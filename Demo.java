8c4bcdd75b95e50761543d6c1a56f20cb5267b01
imtanmayraj <tanmaymth@gmail.com>
2024-10-03 15:00:30 +0530

    comparator vs comparable

--- Demo.java
+++ Demo.java
@@ -1,25 +1,89 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Hashtable;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Comparator;
+
+
+//class Student implements Comparable<Student>
+class Student
+{
+	int age;
+	String name;
+	
+	public Student(int age, String name)
+	{
+		this.age=age;
+		this.name=name;
+	}
+	
+	public String toString() {
+		return "Student [age=" + age + ", name=" +name +"]";
+	}
+	
+//	public int CompareTo(Student that)
+//	{
+////		return 0;
+//		if(this.age >that.age)
+//			return 1;
+//		else
+//			return -1;
+//	}
+}
+	
 
 public class Demo {
     public static void main(String[] args){   
     	
-//    	Map<String, Integer> students=new HashMap<>();
-    	Map<String, Integer> students=new Hashtable<>();
+//    	Comparator<Integer> com=new Comparator<Integer>()
+//    	{
+//    		public int compare(Integer i,Integer j)
+//    		{
+//    			if(i%10 >j%10)
+//    				return 1;
+//    			else
+//    				return -1;
+//    		}
+//    	};	
     	
-    	students.put("Tanmay",92);
-    	students.put("Tushar",23);
-    	students.put("Mayur",67);
-    	students.put("Mohit",56);
-    	students.put("Mohit",45);
+//    	List<Integer> nums= new ArrayList<>();
+//    	nums.add(43);
+//    	nums.add(31);
+//    	nums.add(72);
+//    	nums.add(29);
     	
-    	System.out.println(students.keySet());
+   	Comparator<Student> com=new Comparator<Student>()
+   	{
+   		public int compare(Student i,Student j)
+   		{
+   			if(i.age >j.age)
+   				return 1;
+   			else
+   				return -1;
+   		}
+   	};	
     	
-    	for(String key : students.keySet())
-    	{
-    		System.out.println(key + ":" + students.get(key));
-    	}
+    	// Comparator<Student> com=(i,j) -> i.age > j.age?1:-1;
+     	
+    	List<Student> studs= new ArrayList<>();
+    	studs.add(new Student(21,"Tanmay"));
+    	studs.add(new Student(12,"Tushar"));
+    	studs.add(new Student(18,"Mohit"));
+    	studs.add(new Student(20,"Mayur"));
     	
+//    	Collections.sort(nums);
+//    	System.out.println(nums);
+    	
+    	// for(Student s:studs)
+    	// 	System.out.println(s);
+    	
+    	Collections.sort(studs,com);
+    	for(Student s: studs)
+    		System.out.println(s);
     }
 }
+
+
+
+
+    
+
