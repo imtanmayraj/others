71c78e73ce0d1aee909cb9188b12066cec9e58bf
imtanmayraj <tanmaymth@gmail.com>
2024-10-03 12:10:31 +0530

    colections

--- Demo.java
+++ Demo.java
@@ -1,8 +1,4 @@
-- New State   
-- Runnable State  ->  start() method
-- Running State     -> a thead is running with run() method 
-- Waiting State	    -> sleep(),  wait() method
-- Dead State
-
-Through notify(), you will go to waiting state to runnable state.
-From Running, Runnable state to dead state through stop() method.
+Collection API -> concept
+Collection -> Interface
+Collections -> classe with multiple methods
+			    different type of data structures
