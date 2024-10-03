56d3786ff9274e511f0551f0b8009254085c4f00
imtanmayraj <tanmaymth@gmail.com>
2024-10-03 12:31:08 +0530

    ArrayList

--- Demo.java
+++ Demo.java
@@ -1,4 +1,31 @@
-Collection API -> concept
-Collection -> Interface
-Collections -> classe with multiple methods
-			    different type of data structures
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Demo {
+    public static void main(String[] args){   
+    	
+//    	Collection<Integer> nums= new ArrayList<Integer>();
+//  	Collection nums=new ArrayList();
+    	List<Integer> nums=new ArrayList<Integer>();
+    	nums.add(6);
+    	nums.add(5);
+    	nums.add(8);
+    	nums.add(2);
+    	//nums.add("5");
+    	
+    	System.out.println(nums.get(2));
+    	
+    	System.out.println(nums.indexOf(3));
+    	
+//    	for(int n:nums)
+//    	{
+//    		System.out.println(nums);	
+//    	}
+    	// for(Object n:nums)
+    	// {
+    	// 	int num=(Integer)n;
+    	// 	System.out.println(nums);	
+    	// }
+    }
+}
