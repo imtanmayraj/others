8152070ccae98887e7d8b0c288c54da73e003704
imtanmayraj <tanmaymth@gmail.com>
2024-10-03 10:55:09 +0530

    multiple thread

--- Demo.java
+++ Demo.java
@@ -1,7 +1,36 @@
-Threads:-
-Multiple threads run at same time in a code.
-This is known as Multithreading.
+class A extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hi");
+		}
+	}
+}
 
-- A thread is a smallest unit to work with. (individual task)
-- They can run parallely.
-- Multiple threads can share resources.
+class B extends Thread
+{
+	public void run()
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println("Hello");
+		}
+	}
+}
+
+public class Demo {
+    public static void main(String[] args) throws NumberFormatException {   
+    	
+    	A obj1=new A();
+    	B obj2=new B();
+    	
+//    	obj1.show();
+//    	obj2.show();
+    	
+    	obj1.start();
+    	obj2.start();
+    }
+    
+}
