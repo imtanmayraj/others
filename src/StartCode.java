5167c412707f6751ac1bb8331ba9481b41887835
imtanmayraj <tanmaymth@gmail.com>
2024-10-18 09:44:48 +0530

    untype safe example

--- src/StartCode.java
+++ src/StartCode.java
@@ -20,13 +20,13 @@ public class StartCode {
         */
 
         //Type safe collection
-        ArrayList<String> names = new ArrayList<String>();
-        names.add("Durgesh");
-        names.add("Sitara");
-        names.add("Roshni");
-        names.add("Vandna");
-        names.add("Vandna");
-        System.out.println(names);
+//        ArrayList<String> names = new ArrayList<String>();
+//        names.add("Durgesh");
+//        names.add("Sitara");
+//        names.add("Roshni");
+//        names.add("Vandna");
+//        names.add("Vandna");
+//        System.out.println(names);
 
         //get
 //        System.out.println(names.get(0));
@@ -34,60 +34,60 @@ public class StartCode {
 
 
         //un type safe
-//        LinkedList list = new LinkedList();
-//        list.add("Sachin");
-//        list.add(101);
-//        list.add(625.5124);
-//        list.add(true);
-//        System.out.println(list);
+        LinkedList list = new LinkedList();
+        list.add("Sachin");
+        list.add(101);
+        list.add(625.5124);
+        list.add(true);
+        System.out.println(list);
 
         //remove
-        names.remove("Roshni");
-
-        System.out.println(names);
-
-        //size
-
-        System.out.println("SIZE = " + names.size());
-
-        //check item is there or not
-        // Type ka equals method call karta hai checking karne ke lie
-        //Student
-        System.out.println(names.contains("Durgesh Kumar"));
-
-        //check for empy
-        System.out.println(names.isEmpty());
-
-        //setting value..
-        //names.set(1,"Ram");
-        names.add(1, "Ram");
-
-        System.out.println(names);
-
-        //remove all elements
-//        names.clear();
-
-        System.out.println(names);
-
-        Vector<String> vector = new Vector<>();
-        vector.addAll(names);
-
-        System.out.println("VECTOR " + vector);
-
-        System.out.println("_____________________________________");
-
-        HashSet<Double> nms = new HashSet<>();
-        nms.add(14.14);
-        nms.add(34.12354);
-        nms.add((2354.235));
-        nms.add(99.3);
-        nms.add(99.3);
-        nms.add(5.3);
-        System.out.println(nms);
-
-        TreeSet<Double> tset = new TreeSet<>();
-        tset.addAll(nms);
-        System.out.println(tset);
+//        names.remove("Roshni");
+//
+//        System.out.println(names);
+//
+//        //size
+//
+//        System.out.println("SIZE = " + names.size());
+//
+//        //check item is there or not
+//        // Type ka equals method call karta hai checking karne ke lie
+//        //Student
+//        System.out.println(names.contains("Durgesh Kumar"));
+//
+//        //check for empy
+//        System.out.println(names.isEmpty());
+//
+//        //setting value..
+//        //names.set(1,"Ram");
+//        names.add(1, "Ram");
+//
+//        System.out.println(names);
+//
+//        //remove all elements
+////        names.clear();
+//
+//        System.out.println(names);
+//
+//        Vector<String> vector = new Vector<>();
+//        vector.addAll(names);
+//
+//        System.out.println("VECTOR " + vector);
+//
+//        System.out.println("_____________________________________");
+//
+//        HashSet<Double> nms = new HashSet<>();
+//        nms.add(14.14);
+//        nms.add(34.12354);
+//        nms.add((2354.235));
+//        nms.add(99.3);
+//        nms.add(99.3);
+//        nms.add(5.3);
+//        System.out.println(nms);
+//
+//        TreeSet<Double> tset = new TreeSet<>();
+//        tset.addAll(nms);
+//        System.out.println(tset);
 
 
     }
